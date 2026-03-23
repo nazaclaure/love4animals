668ff17fac615a1cfa2815fced1300380cd9f12d
Nazaret <nazaret.claure@ucb.edu.bo>
2026-03-22 20:15:25 -0400

    add postman collection

--- Project1202/Dto/GetProductDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Project1202.Dto;
-
-public record GetProductDto (
-    string Code,
-    ProductDto Product,
-    int Status,
-    string Status_verbose
-);
--- Project1202/Dto/OrderProductDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Project1202.Dto;
-
-public record OrderProductDto(
-    string Code,
-    string Name,
-    int Quantity,
-    double? Total
-);
